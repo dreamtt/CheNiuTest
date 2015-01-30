8f9aeff3b0767338811ad940f75976b912c2d6cd
dreamtt <1026102055@qq.com>
2015-01-30 10:59:21 +0800

    wrote a .h file

---
+++ NotificationDefines.h
@@ -0,0 +1,10 @@
+//
+//  NotificationDefines.h
+//  yiqiba
+//
+//  Created by taotao on 14-9-29.
+//  Copyright (c) 2014年 taotao. All rights reserved.
+//
+
+
+#define RECEIVED_MESSAGE @"ReceivedMessage"
